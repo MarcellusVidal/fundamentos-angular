a949e24de5f592b32bd29ba0f7fc789905bcd084
Marcellus <arquiteto.marcellus@gmail.com>
2022-08-31 07:02:14 -0300

    Update  - Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { PipiCustomizadoComponent } from './pipi-customizado/pipi-customizado.co
 import { TwoWayDateBindingComponent } from './two-way-date-binding/two-way-date-binding.component';
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
+import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
+import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
+import { ExemploServicos1Component } from './exemplo-servicos1/exemplo-servicos1.component';
+import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2.component';
+import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { CicloDeVidaPaiComponent } from './ciclo-de-vida-pai/ciclo-de-vida-pai.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +26,13 @@ import { RenderizandoListasComponent } from './renderizando-listas/renderizando-
     PipiCustomizadoComponent,
     TwoWayDateBindingComponent,
     RenderizandoListasComponent,
+    ComponentePersonalizadoComponent,
+    ComponentePaiComponent,
+    ComponenteFilhoComponent,
+    ExemploServicos1Component,
+    ExemploServicos2Component,
+    CicloDeVidaComponent,
+    CicloDeVidaPaiComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
